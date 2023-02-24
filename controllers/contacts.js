4dd66fcb4734ef9012d915d6320c16c1a337dba2
Comondear <elcomondear@gmail.com>
2023-02-24 13:04:56 -0700

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
 // const {
 //   listContacts,
@@ -11,13 +11,13 @@ const contacts = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.getContactById(id);
+  const result = await Contact.findById(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -25,13 +25,22 @@ const getById = async (req, res) => {
 };
 
 const postContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const putContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const patchFavorite = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -40,7 +49,7 @@ const putContact = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.removeContact(id);
+  const result = await Contact.findByIdAndDelete(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -52,5 +61,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   postContact: ctrlWrapper(postContact),
   putContact: ctrlWrapper(putContact),
+  patchFavorite: ctrlWrapper(patchFavorite),
   deleteById: ctrlWrapper(deleteById),
 };
--- middlewapes/index.js
+++ middlewapes/index.js
@@ -1,5 +1,7 @@
 const validateBody = require("../middlewapes/validateBody");
+const isValidId = require("./isValidId");
 
 module.exports = {
   validateBody,
+  isValidId,
 };
